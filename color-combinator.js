d9d797e8d0b2cf0cdb1eb4094faea91d20ecd00a
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-02-25 12:35:26 -0500

    Fixes console.logs to returns in color-combinator.js

--- color-combinator.js
+++ color-combinator.js
@@ -2,24 +2,19 @@
  * YOUR CODE HERE *
  ******************/
 
-const color1 = getInput(1);
-const color2 = getInput (2);
-
 function colorCombinator(color1, color2) {
   if (color1 === 'red' && color2 === 'yellow') {
-    console.log('orange');
+    return 'orange';
   } else if (color1 === 'yellow' && color2 === 'red'){
-    console.log('orange');
+    return 'orange';
   } else if (color1 === 'red' && color2 === 'blue') {
-    console.log('purple');
+    return 'purple';
   } else if (color1 === 'blue' && color2 === 'red'){
-    console.log('purple');
+    return 'purple';
   } else if (color1 === 'yellow' && color2 === 'blue') {
-    console.log('green');
+    return 'green';
   } else if (color1 === 'blue' && color2 === 'yellow'){
-    console.log('green');
-  } else {
-    console.log('error');
+    return 'green';
   }
 }
 
