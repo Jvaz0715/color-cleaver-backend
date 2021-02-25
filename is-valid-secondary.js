e3b9948727447eab3243357f73981460c9c30db0
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-02-25 11:50:29 -0500

    Adds isValidSecondary function

--- is-valid-secondary.js
+++ is-valid-secondary.js
@@ -2,6 +2,20 @@
  * YOUR CODE HERE *
  ******************/
 
+const color1 = getInput(1);
+const color2 = getInput (2);
+
+function isValidSecondary() {
+  if (color1 === 'orange' || color2 === undefined) {
+    console.log('orange');
+  } else if (color1 === 'purple' || color2 === undefined){
+    console.log('purple');
+  } else if (color1 === 'green' || color2 === undefined){
+    console.log('green');
+  } else {
+    console.log('That is not a secondary color');
+  }
+}
 
 
 
