04acd1145de1ae8cb169e6898eed3a6e1df30ba8
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-02-25 11:47:07 -0500

    Adds isValidPrimary function

--- is-valid-primary.js
+++ is-valid-primary.js
@@ -2,6 +2,20 @@
  * YOUR CODE HERE *
  ******************/
 
+const color1 = getInput(1);
+const color2 = getInput (2);
+
+function isValidPrimary() {
+  if (color1 === 'red' || color2 === 'red') {
+    console.log('red');
+  } else if (color1 === 'yellow' || color2 === 'yellow'){
+    console.log('green');
+  } else if (color1 === 'blue' || color2 === 'blue'){
+    console.log('blue');
+  } else {
+    console.log('That is not a primary color');
+  }
+}
 
 
 
